7bd98344d78c572888f78e58f71870bbb8adde1f
GeriCookie <Gergana.bailova@gmail.com>
2016-02-07 16:49:44 +0200

    Added "npm test", fixed add/remove from watch list

--- ShowWatchlistWeb/Routes/watchRoutes.js
+++ ShowWatchlistWeb/Routes/watchRoutes.js
@@ -9,6 +9,28 @@ let auth = function() {
   });
 };
 
+let convertToShowToWatch = function(show) {
+  let userShow = {
+    _id: show._id,
+    title: show.title,
+    seasons: show.seasons.map(season => {
+      let episodes = season.episodes.map(episode => {
+        return {
+          title: episode.title,
+          isWatched: false
+        };
+      });
+      return {
+        id: season.id,
+        title: season.title,
+        isWatched: false,
+        episodes: episodes
+      };
+    })
+  };
+  return userShow;
+};
+
 var routes = function(Show, Update, User) {
   let router = new express.Router();
 
@@ -23,47 +45,48 @@ var routes = function(Show, Update, User) {
     put: function(req, res) {
       let user = req.user;
       let showId = req.body.showId;
-      Show.findById(showId)
-        .exec(function(err, show) {
-          if (err) {
-            return res.status(404)
-              .send({
-                message: 'Invalid show Id'
-              });
-          }
-          if (!user.showsToWatch) {
-            user.showsToWatch = [];
-          }
 
-          let userShow = {
-            _id: show._id,
-            title: show.title,
-            seasons: show.seasons.map(season => {
-              let episodes = season.episodes.map(episode => {
-                return {
-                  title: episode.title,
-                  isWatched: false
-                };
-              });
-              return {
-                id: season.id,
-                title: season.title,
-                isWatched: false,
-                episodes: episodes
-              };
-            })
-          };
-          user.showsToWatch.push(userShow);
-          user.save(function(err) {
-            if (err) {
-              return res.status(400)
-                .send({
-                  message: 'Invalid request'
-                });
-            }
-            return res.send(user);
+      let userShows = user.showsToWatch || [];
+      let indexOfShowInUser = userShows.findIndex(sh => sh._id.toString() === showId);
+      if (indexOfShowInUser >= 0) {
+        let removedShow = user.showsToWatch.splice(indexOfShowInUser, 1)[0];
+        user.save(function(err) {
+          if (err) {
+            return res.status(400)
+              .send(err);
+          }
+          return res.send({
+            message: `${removedShow.title} removed from user's watch list`
           });
         });
+      } else {
+        Show.findById(showId)
+          .exec(function(err, show) {
+            if (err) {
+              return res.status(404)
+                .send({
+                  message: 'Invalid show Id'
+                });
+            }
+            if (!user.showsToWatch) {
+              user.showsToWatch = [];
+            }
+
+            let userShow = convertToShowToWatch(show);
+            user.showsToWatch.push(userShow);
+            user.save(function(err) {
+              if (err) {
+                return res.status(400)
+                  .send({
+                    message: 'Invalid request'
+                  });
+              }
+              return res.send({
+                message: `${show.title} added to user's watch list`
+              });
+            });
+          });
+      }
     }
   };
 
